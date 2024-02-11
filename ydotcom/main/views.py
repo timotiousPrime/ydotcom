5d293997b8d5ffdbe64e3d2f909a587177ad1cd8
timotiousPrime <timodb031@gmail.com>
2024-02-11 08:19:42 +0200

    initial url, view and template for main app

--- ydotcom/main/views.py
+++ ydotcom/main/views.py
@@ -1,3 +1,13 @@
+# from django.shortcuts import render
+from django.views.generic import View
 from django.shortcuts import render
 
+
 # Create your views here.
+class landingPage(View):
+    template_name = "main/landingPage.html"
+    title = "Hello, World. Welcome to the landing Page"
+
+    def get(self, request):
+        context = {"title": self.title}
+        return render(request, self.template_name, context)

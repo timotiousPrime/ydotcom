135229263dcb3ae32d5dd4e0dcadfaaad7809c2e
timotiousPrime <timodb031@gmail.com>
2024-02-11 08:49:28 +0200

    add login, logout, register urls

--- ydotcom/accounts/urls.py
+++ ydotcom/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import accountsPage
+from .views import accountsPage, loginPage, logoutPage, registerPage
 
 
 urlpatterns = [
     path("", accountsPage.as_view(), name="accountsPage"),
+    path("login/", loginPage.as_view(), name="login"),
+    path("logout/", logoutPage.as_view(), name="logout"),
+    path("register/", registerPage.as_view(), name="register"),
 ]

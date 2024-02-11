2b0254f2f0d26ede44941c0e3e1df5c5cce0fdcb
timotiousPrime <timodb031@gmail.com>
2024-02-11 08:50:35 +0200

    initial model for posts - no migrations made. WIP

--- ydotcom/posts/models.py
+++ ydotcom/posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete= models.CASCADE,
+                             related_name= "posts",
+                             blank= False,
+                             null= False,
+                             )
+    timestamp= models.DateTimeField(blank=False,
+                                     null= False,
+                                     )
+    message= models.TextField(max_length= 140,
+                              blank=False,
+                              null= False,
+                              )
+    
+    def __str__(self):
+        return f"{self.user} @ {self.timestamp}: {self.message}"

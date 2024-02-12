00c89630c19616182f7bf7ccf9736b6828bcff1a
timotiousPrime <timodb031@gmail.com>
2024-02-12 05:50:11 +0200

    update url names

--- ydotcom/accounts/urls.py
+++ ydotcom/accounts/urls.py
@@ -3,8 +3,8 @@ from .views import accountsPage, loginPage, logoutPage, registerPage
 
 
 urlpatterns = [
-    path("", accountsPage.as_view(), name="accountsPage"),
-    path("login/", loginPage.as_view(), name="login"),
-    path("logout/", logoutPage.as_view(), name="logout"),
-    path("register/", registerPage.as_view(), name="register"),
+    path("", accountsPage.as_view(), name="Accounts_Page"),
+    path("login/", loginPage.as_view(), name="Login"),
+    path("logout/", logoutPage.as_view(), name="Logout"),
+    path("register/", registerPage.as_view(), name="Register"),
 ]

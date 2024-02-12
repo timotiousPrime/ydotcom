b0609a560d253faa4b9502667e86a2d730a11af6
timotiousPrime <timodb031@gmail.com>
2024-02-12 05:52:03 +0200

    update url names for home and landing page

--- ydotcom/main/urls.py
+++ ydotcom/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import landingPage
+from django.contrib.auth.decorators import login_required
+from .views import landingPage, homePage
 
 
 urlpatterns = [
-    path("", landingPage.as_view(), name="Home"),
+    path("", landingPage.as_view(), name="LandingPage"),
+    path("home/", login_required(homePage.as_view()), name="HomePage"),
 ]

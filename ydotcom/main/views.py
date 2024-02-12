a95285ed2df7412e5e5a539b670f33fe605e2c41
timotiousPrime <timodb031@gmail.com>
2024-02-12 05:52:38 +0200

    update homepage view

--- ydotcom/main/views.py
+++ ydotcom/main/views.py
@@ -1,13 +1,29 @@
 # from django.shortcuts import render
 from django.views.generic import View
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 
 # Create your views here.
+
+# redirect to login page or user home
 class landingPage(View):
     template_name = "main/landingPage.html"
     title = "Hello, World. Welcome to the landing Page"
 
     def get(self, request):
         context = {"title": self.title}
+        return render(request, self.template_name, context)
+    
+
+class homePage(View):
+    template_name = "main/homePage.html"
+    title = "Home Page"
+    users = User.objects.all()
+
+    def get(self, request):
+        context = {"title": self.title,
+                   "users": self.users,
+                   }
         return render(request, self.template_name, context)

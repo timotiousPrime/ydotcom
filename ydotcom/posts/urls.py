6572fbe7e787a1c641cee018f567039a079f6ac6
timotiousPrime <timodb031@gmail.com>
2024-02-12 05:56:12 +0200

    update url name for Post view

--- ydotcom/posts/urls.py
+++ ydotcom/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import postsPage
+from django.contrib.auth.decorators import login_required
+from .views import User_Posts
 # from . import views
 
 urlpatterns = [
-    path("posts", postsPage.as_view(), name="posts"),
+    path("<str:username>/", login_required(User_Posts.as_view()), name="User_Posts"),
 ]

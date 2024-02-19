5558b78a053e09ac4f852e9d89863707aab460c3
timotiousPrime <timodb031@gmail.com>
2024-02-19 18:46:59 +0200

    replace class based views with function based view

--- ydotcom/posts/views.py
+++ ydotcom/posts/views.py
@@ -1,37 +1,51 @@
-from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from django.contrib.auth.models import User
 from .forms import PostForm
-
+from django.contrib.auth.decorators import login_required
 from collections import defaultdict 
 
 
 # Create your views here.
-class User_Posts(View):
-    template_name= "posts/userPosts.html"
 
-    def get(self, request, username):
-        # Get users and users posts and order by date
+@login_required
+def userPostsList(request, username):
+    if request.method == "GET":
         user = User.objects.get(username=username)
-        posts = Post.objects.filter(user=user).order_by('-timestamp').values()
-        
+        posts = Post.objects.filter(user = user.id)
+        form = PostForm()
+
         # group posts per day 
         qs = defaultdict(list)
         for query in posts:
-            post_date = query['timestamp'].date
-            qs[post_date] += [query['message']]
+            post_date = query.timestamp.date
+            qs[post_date] += [query.message]
         qs = dict(qs)
 
-        context = {"title": f"{ username }'s Posts",
-                   "posts": qs,
-                   "poster": user}
-        
-        return render(request, self.template_name, context)
-    
-    def post(self, request):
+        context = {
+            "title": username,  
+            "posts": qs,
+            "poster": User.objects.get(username=username),
+            "form": form
+        }
+        return render(request, "posts/userPosts.html", context)
+
+@login_required
+def createPost(request, username):
+    user = get_object_or_404(User, username=request.user.username)
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("User_Posts", request.user)
-        return render(request, self.template_name, {"form": PostForm()})
+            post = form.save(commit=False)
+            post.user = user
+            post.save()
+            return redirect("User_Posts", user.username)
+    else:
+        form = PostForm()
+
+    context = {
+        "form": form,
+        "user": user
+    }
+    print(context['form'])
+    return render(request, "posts/userPosts.html", context)

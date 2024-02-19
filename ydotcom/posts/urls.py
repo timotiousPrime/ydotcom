9d58bb09d55e4c02b6b32d5ad7e7403517454d7e
timotiousPrime <timodb031@gmail.com>
2024-02-19 18:56:39 +0200

    update urls for FBV

--- ydotcom/posts/urls.py
+++ ydotcom/posts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import User_Posts
-# from . import views
+from . import views
 
 urlpatterns = [
-    path("<str:username>/", login_required(User_Posts.as_view()), name="User_Posts"),
+    path("<str:username>/", login_required(views.userPostsList), name="User_Posts"),
+    path("<str:username>/create", login_required(views.createPost), name="New_Post"),
 ]

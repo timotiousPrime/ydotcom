833535d8499d4ae9348f870914931beae1dd53a2
timotiousPrime <timodb031@gmail.com>
2024-02-22 00:32:23 +0200

    update url names

--- ydotcom/accounts/urls.py
+++ ydotcom/accounts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import accountsPage, loginPage, logoutPage, registerPage
+from .views import loginPage, logoutPage, registerPage
 
 
 urlpatterns = [
-    path("", accountsPage.as_view(), name="Accounts_Page"),
     path("login/", loginPage.as_view(), name="Login"),
     path("logout/", logoutPage.as_view(), name="Logout"),
     path("register/", registerPage.as_view(), name="Register"),

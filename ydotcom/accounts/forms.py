9c221c3b1ba633989b125716bb3914fbc9d3cf65
timotiousPrime <timodb031@gmail.com>
2024-02-27 19:49:51 +0200

    add forms for user profile and employment history

--- ydotcom/accounts/forms.py
+++ ydotcom/accounts/forms.py
@@ -1,15 +1,43 @@
 # Models
 from django.contrib.auth.models import User
-
+from .models import UserProfile, EmploymentHistory
 # Forms
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.forms import inlineformset_factory
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    first_name = forms.CharField(max_length=24, strip=True, required=True)
-    last_name = forms.CharField(max_length=24, strip=True, required=True)
     class meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('title', 'date_of_birth', 'phone_number', 'interests' )
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+            'interests': forms.CheckboxSelectMultiple
+        }
+
+
+class EmploymentHistoryForm(forms.ModelForm):
+    class Meta:
+        model = EmploymentHistory
+        fields = ('start_date', 'end_date', 'organisation_name', 'position')
+        widgets = {
+            'start_date': forms.DateInput(attrs={'type':'date'}),
+            'end_date': forms.DateInput(attrs={'type':'date'}),
+            'position': forms.CheckboxSelectMultiple(),
+        }
+
+
+# Define formset so I can manage multiple employment history records relative to a single user
+EmploymentHistoryFormSet = inlineformset_factory(
+    UserProfile, EmploymentHistory, 
+    form = EmploymentHistoryForm,
+    extra=1, can_delete=True
+)

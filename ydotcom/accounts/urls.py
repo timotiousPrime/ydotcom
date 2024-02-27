e6e7e80349ab4f071d46dd764cc30aa6c55a06eb
timotiousPrime <timodb031@gmail.com>
2024-02-27 19:52:57 +0200

    add urls routes for API views

--- ydotcom/accounts/urls.py
+++ ydotcom/accounts/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
-from .views import loginPage, logoutPage, registerPage
+from django.urls import path, include
+from .views import loginPage, logoutPage, registerPage, UserProfileViewSet, InterestViewSet, EmploymentHistoryViewSet, UserProfileUpdateView
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'users', UserProfileViewSet, basename='user')
+router.register(r'interest', InterestViewSet)
+router.register(r'employment', EmploymentHistoryViewSet)
 
 urlpatterns = [
     path("login/", loginPage.as_view(), name="Login"),
     path("logout/", logoutPage.as_view(), name="Logout"),
     path("register/", registerPage.as_view(), name="Register"),
+    path("profile/edit/<str:username>", UserProfileUpdateView.as_view(), name="Edit_Profile"),
+
+    path('api/', include (router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]

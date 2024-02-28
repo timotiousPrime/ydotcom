81df23956321e0ff68e7ce85b8bd317cdfac6d92
timotiousPrime <timodb031@gmail.com>
2024-02-29 00:35:23 +0200

    add new routes

--- ydotcom/accounts/urls.py
+++ ydotcom/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import loginPage, logoutPage, registerPage, UserProfileViewSet, InterestViewSet, EmploymentHistoryViewSet, UserProfileUpdateView
+from .views import loginPage, logoutPage, registerPage, UserProfileViewSet, UsersViewSet, InterestViewSet, EmploymentHistoryViewSet, newAccountApiView
 from rest_framework.routers import DefaultRouter
+from . import views
 
 router = DefaultRouter()
-router.register(r'users', UserProfileViewSet, basename='user')
+router.register(r'user', UsersViewSet)
+router.register(r'profile', UserProfileViewSet, basename='profile')
 router.register(r'interest', InterestViewSet)
 router.register(r'employment', EmploymentHistoryViewSet)
 
@@ -11,8 +13,8 @@ urlpatterns = [
     path("login/", loginPage.as_view(), name="Login"),
     path("logout/", logoutPage.as_view(), name="Logout"),
     path("register/", registerPage.as_view(), name="Register"),
-    path("profile/edit/<str:username>", UserProfileUpdateView.as_view(), name="Edit_Profile"),
-
+    path("test/", views.testView, name="TestView"),
+    path("user_account/", newAccountApiView.as_view(), name="newAccount"),
     path('api/', include (router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]

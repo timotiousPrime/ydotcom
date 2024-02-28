619c6281f6bbcd027c346c3a8d440e2c0859db08
timotiousPrime <timodb031@gmail.com>
2024-02-29 00:35:12 +0200

    add user serialiser

--- ydotcom/accounts/serializers.py
+++ ydotcom/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import UserProfile, Interest, EmploymentHistory, JobTitle
+from django.contrib.auth.models import User
 
 class EmploymentHistorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,14 +13,22 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserProfile
-        fields = ['user', 'title', 'first_name', 'surname', 'date_of_birth', 'phone_number', 'employment_history']
+        fields = ['pk', 'user', 'title', 'first_name', 'surname', 'date_of_birth', 'phone_number', 'employment_history', 'interests']
 
     def create(self, validated_data):
-        employment_data = validated_data.pop('employment')
+        employment_data = validated_data.pop('employment_history')
         user_profile = UserProfile.objects.create(**validated_data)
         for employment in employment_data:
             EmploymentHistory.objects.create(user_profile=user_profile, **employment)
         return user_profile
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+
 
 class InterestSerializer(serializers.ModelSerializer):
 
@@ -33,3 +42,12 @@ class JobTitleSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobTitle
         fields = '__all__'
+
+
+class UserAccountSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(many=False)
+    employment_history = EmploymentHistorySerializer(source='employeehistory_set', many=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'profile', 'employment_history']

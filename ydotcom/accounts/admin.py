887b9141ef4cb103516bd70df994e6ab56a60213
timotiousPrime <timodb031@gmail.com>
2024-03-04 00:34:34 +0200

    remove unneeded models

--- ydotcom/accounts/admin.py
+++ ydotcom/accounts/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import Interest, JobTitle, UserProfile, EmploymentHistory
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
-# Extend User Model
 
 class EmploymentHistoryInline(admin.StackedInline):
     model = EmploymentHistory
@@ -11,29 +9,24 @@ class EmploymentHistoryInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = 'employment history'
 
-# User Profile admin
-class UserProfileAdmin(admin.ModelAdmin):
-    inlines = (EmploymentHistoryInline,)
-
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
     verbose_name_plural = 'user profile'
-    inlines = [EmploymentHistoryInline,]
+
 
 class UserAdmin(BaseUserAdmin):
-    inlines = [UserProfileInline,]
-
+    model = User
+    inlines = [UserProfileInline, EmploymentHistoryInline]
 
 # Unregister initial User so we can update the modified User for admin
 admin.site.unregister(User)
-
+admin.site.unregister(Group)
 # Reregister User and profile
 admin.site.register(User, UserAdmin)
 
 # register the UserProfileAdmin that will display the employment history included
-admin.site.register(UserProfile, UserProfileAdmin)
 
 admin.site.register(Interest)
 admin.site.register(JobTitle)

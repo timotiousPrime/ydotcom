e6648388406a0ffbd6c29d489614a345bf386e5f
timotiousPrime <timodb031@gmail.com>
2024-03-04 00:38:43 +0200

    add url name

--- ydotcom/posts/urls.py
+++ ydotcom/posts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 from . import views
 
+app_name = "posts"
+
 urlpatterns = [
     path("<str:username>/", login_required(views.userPostsList), name="User_Posts"),
     path("<str:username>/create", login_required(views.createPost), name="New_Post"),
